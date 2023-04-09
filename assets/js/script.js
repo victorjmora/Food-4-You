42251749cbc1276636250ed3e64f4629a69464a8
Anurag Mishra <mildlyadequate69@gmail.com>
2023-04-09 16:33:48 -0400

    user data collection done

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,53 @@ var minLongitude = 'bl_longitude=0';
 var maxLongitude = 'tr_longitude=0';
 var info = [];
 var restaurants = [];
+var zipcode = 0;
+var cuisineOptions = ['Anything', 'Asian', 'American', 'Italian', 'Mexican']
+var cuisineChoice = cuisineOptions[0];
 
-//Fetch Travel Advisor API. If the key isn't working you might have to replace it with a new one
-function setPosition(position) {
+let map;
+
+//Creates the map
+async function initMap() {
+    //@ts-ignore
+    const { Map } = await google.maps.importLibrary("maps");
+
+    map = new Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+}
+//initMap();
+
+//Adds an event listener to the button so that it can collect user input and use it to customize the fetch request
+var submitButton = document.getElementById('submit-button');
+
+submitButton.addEventListener('click', submitInput);
+
+function submitInput() {
+    zipcode = document.getElementById('input_text').value;
+    var value = document.getElementById('test').value;
+    cuisineChoice = cuisineOptions[value];
+    console.log(cuisineChoice)
+    console.log(zipcode)
+}
+
+//Sets up a square boundary based on user's current latitude and longitude and adds it to the link
+function setBoundaries() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setPosition);
+    } else {
+        return '0';
+    }
+}
+
+//Converts zipcode to longitude and latitude and changes the fetch link
+function setPositionZipCode() {
+     
+}
+
+//Sets long/lat to current position and changes the fetch link
+function setPositionCurrent(position) {
     minLatitude = `bl_latitude=${position.coords.latitude - 1}`;
     maxLatitude = `tr_latitude=${position.coords.latitude + 1}`;
     minLongitude = `bl_longitude=${position.coords.longitude - 1}`;
@@ -16,8 +60,7 @@ function setPosition(position) {
     storeInfo();
 }
 
-setBoundaries();
-
+//Fetch Travel Advisor API. If the key isn't working you might have to replace it with a new one
 const options = {
     method: 'GET',
     headers: {
@@ -32,16 +75,7 @@ function fetchAPI() {
         .catch(err => console.error(err));
 }
 
-//Sets up a square boundary based on user's current latitude and longitude and adds it to the link
-function setBoundaries() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(setPosition);
-    } else {
-        return '0';
-    }
-}
-
-//Stores API info in the 'info' variable in order to manipulate
+//Stores API info in the 'info' variable in order to manipulate. Pushes each Restaurant object to the 'restaurants' array with data to be used in the modals
 async function storeInfo() {
     info = await fetch(link, options).then(response => response.json())
     for(var i = 0; i < info.data.length; i++) {
@@ -73,12 +107,13 @@ async function storeInfo() {
     console.log(restaurants);
 };
 
+//Creates a modal for displaying each of the Restaurant objects
+function createModals() {
+    document.addEventListener('DOMContentLoaded', function () {
+        var elems = document.querySelectorAll('.modal');
+        var instances = M.Modal.init(elems, options);
+    });
+}
 
-//Attach to an event listener when the user submits current location. Currently only has a boundary of 2x2 degrees and is not customizable yet. Might use slider to adjust bounds.
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
-  });
 
-setBoundaries();
-storeInfo();
+
